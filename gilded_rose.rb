1da72f6900b990884b17a144c687861317eac595
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-08-04 01:30:12 +0200

    refactor - changed update_quality to a case statement

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -5,23 +5,6 @@ class GildedRose
     @items = items
   end
 
-  # def update_quality()
-  #   @items.each do |item|
-  #     if item.name == "Normal Item"
-  #       normal_update(item)
-  #     end
-  #     if item.name == "Aged Brie"
-  #       brie_update(item)
-  #     end
-  #     if item.name == "Sulfuras, Hand of Ragnaros"
-  #       sulfuras_update(item)
-  #     end
-  #     if item.name == "Backstage passes to a TAFKAL80ETC concert"
-  #       backstage_update(item)
-  #     end
-  #   end
-  # end
-
   def update_quality()
     @items.each do |item|
       case item.name
@@ -32,54 +15,6 @@ class GildedRose
       end
     end
   end  
-
-
-    # @items.each do |item|
-    #   if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-    #     if item.quality > 0
-    #       if item.name != "Sulfuras, Hand of Ragnaros"
-    #         item.quality = item.quality - 1
-    #       end
-    #     end
-    #   else
-    #     if item.quality < 50
-    #       item.quality = item.quality + 1
-    #       if item.name == "Backstage passes to a TAFKAL80ETC concert"
-    #         if item.sell_in < 11
-    #           if item.quality < 50
-    #             item.quality = item.quality + 1
-    #           end
-    #         end
-    #         if item.sell_in < 6
-    #           if item.quality < 50
-    #             item.quality = item.quality + 1
-    #           end
-    #         end
-    #       end
-    #     end
-    #   end
-    #   if item.name != "Sulfuras, Hand of Ragnaros"
-    #     item.sell_in = item.sell_in - 1
-    #   end
-    #   if item.sell_in < 0
-    #     if item.name != "Aged Brie"
-    #       if item.name != "Backstage passes to a TAFKAL80ETC concert"
-    #         if item.quality > 0
-    #           if item.name != "Sulfuras, Hand of Ragnaros"
-    #             item.quality = item.quality - 1
-    #           end
-    #         end
-    #       else
-    #         item.quality = item.quality - item.quality
-    #       end
-    #     else
-    #       if item.quality < 50
-    #         item.quality = item.quality + 1
-    #       end
-    #     end
-    #   end
-    # end
- 
 
   def normal_update(item)
     item.sell_in -= 1
@@ -97,7 +32,6 @@ class GildedRose
   end
 
   def sulfuras_update(item)
-
   end
 
   def backstage_update(item)
