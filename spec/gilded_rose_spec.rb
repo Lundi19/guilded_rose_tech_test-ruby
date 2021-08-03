abb49c6050dc2b24de0e7696ad6c2b9e01cd8ebf
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-08-03 16:30:41 +0200

    added test items to rspec test and fixed first test

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,11 +2,21 @@ require File.join(File.dirname(__FILE__), '../gilded_rose')
 
 describe GildedRose do
 
+  before do
+    TEST_ITEMS = [
+      Item.new("foo", 0, 0),
+      Item.new("Sake", 10, 20),
+      Item.new("Normal Item", 1, 10),
+      Item.new("Aged Brie", 10, 20),
+      Item.new("Sulfuras, Hand of Ragnaros", 0, 50),
+      Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+    ]
+    @test = GildedRose.new(TEST_ITEMS)
+  end
+
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+      expect(@test.update_quality[0].name).to eq "foo"
     end
   end
 
